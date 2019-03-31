12fed9a6279aa3fa00d494236c033fad3a30d0ce
SergeNanou <serge.nanou@hetic.net>
2019-03-31 08:46:55 +0200

    labyrinthe activate

--- core.py
+++ core.py
@@ -4,6 +4,7 @@
 import pygame
 from pygame.locals import *
 from functions import *
+
 pygame.init()
 
 #Opening of pygame Window
@@ -20,108 +21,105 @@ background = pygame.image.load("ciel-bleu.jpg").convert()
 
 #Mac Gyver element import
 etherPNG = pygame.image.load('ether.png').convert()
-seringuePNG = pygame.image.load('seringue.png').convert()
+seringuePNG = pygame.image.load('seingue_2.png').convert()
 aiguillePNG = pygame.image.load('aiguille.png').convert()
+tubePNG = pygame.image.load('tube_plastique.png').convert()
+image_acceilPNG =  pygame.image.load('acceuil_3.png').convert()
+#goal = pygame.image.load('Gardien.png').convert()
+#image
 
 #Instance of support
 
 home = Support()
 home.create()
+print(home.structure)
+tail_sprite = 30
+#we walk through each line of a structure
 
+				
 #####Instantiate test variables
-
+acceuilTest = True
 etherTest = True
 seringueTest = True
 aiguilleTest = True 
+tubeTest = True
 cpt = 0
-loser = False
+loser = True
 win = True
 ##Instance and generate the elements that permit to Mac Gyver to kill the garden of exit
 
 ether = Element(home, etherPNG)
 ether.generate(etherPNG,opening)
-seringue = Element(home, seringuePNG)
-seringue.generate(seringuePNG, opening)
+tube = Element(home, tubePNG)
+tube.generate(seringuePNG, opening)
 aiguille = Element(home, aiguillePNG)
 aiguille.generate(aiguillePNG, opening)
 
 
 #Creation de Mac Gyver
 Mac_Gyver = Maestro(home)
-continue_game = 1 
+#continue_game = 1 
 
 #BOUCLE INFINIE
 #continuer = False
-continuer_open = True
+continuer = True
 #continue_intro = False
 #intro = True
 #jouer = True
-while continuer_open:
-	continuer_intro = True
+
 	
-	while continuer_intro:
+#direct = pygame.transform.scale((pygame.image.load('MacGyver.png').convert()),(30, 30))					
+			
+while continuer:
 
-		rectScreen = opening.get_rect()
-			#police = pygame.font.Font("led.ttf",72)
-		police = pygame.font.Font(None,10)
-		texte = police.render("THIS GAME CONSISTE TO PERMIT MAC GYVER TO TAKE ANY ELEMENT TO KILL A GARDEN IN A LABYRINTH",True,pygame.Color("#FFFF00"))
-		rectTexte = texte.get_rect()
-		rectTexte.center = rectScreen.center
-		opening.fill(pygame.Color("#FF0000"))
-		opening.blit(texte,rectTexte)
+	while acceuilTest :
 
-		pygame.display.flip()
+			opening.blit((pygame.transform.scale(image_acceilPNG, (450, 450))), (0, 0))
+			for event in pygame.event.get(): #function to catch a user action	
+
+				if event.type == KEYDOWN: # catch a keyboard action
+						                  		
+					#Keys movement of Mac Gyver
+					if event.key == K_ESCAPE:
+						acceuilTest = False
+
+			pygame.display.flip()
+
+
+	for event in pygame.event.get(): #function to catch a user action	
+
+		if event.type == KEYDOWN: # catch a keyboard action
+				                  
 		
-		for event in pygame.event.get(): #function to catch a user action
-				
 					
-			if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
-				continuer_open = False
-				continuer_intro = False
-				continuer = False
+			#Keys movement of Mac Gyver
+			if event.key == K_RIGHT:
+				Mac_Gyver.move('right')
 				
+			if event.key == K_LEFT:
+				Mac_Gyver.move('left')
 				
-			elif event.type == KEYDOWN:				
-				#Lancement du niveau 1
-				if event.key == K_0:
-					continuer_intro = False	#On quitte l'accueil
-					continuer = True
+			if event.key == K_UP:
+				Mac_Gyver.move('top')
+				
+			if event.key == K_DOWN:
+				Mac_Gyver.move('low')
+					
+			elif event.key == K_ESCAPE:# quit the opening
+					continuer = False
+				
+			
+#Construct a labyrinth
+
 	
-					
-			
-	while continuer:
-
-
-		for event in pygame.event.get(): #function to catch a user action	
-
-			if event.type == KEYDOWN: # catch a keyboard action
-					                  
-			
-						
-				#Keys movement of Mac Gyver
-				if event.key == K_RIGHT:
-					Mac_Gyver.move('right')
-					
-				if event.key == K_LEFT:
-					Mac_Gyver.move('left')
-					
-				if event.key == K_UP:
-					Mac_Gyver.move('top')
-					
-				if event.key == K_DOWN:
-					Mac_Gyver.move('low')
-						
-				elif event.key == K_ESCAPE:# quit the opening
-						continuer = False
-					
-				
-	#Construct a labyrinth
-
-		
 	opening.blit((pygame.transform.scale(background, (450, 450))), (0, 0))
+
+	#opening.blit(pygame.transform.scale(goal,(30, 30)), (420,420))
+		
 	home.poster(opening)
 
-	opening.blit(Mac_Gyver.direct, (Mac_Gyver.x, Mac_Gyver.y)) # post Mac gyver and playing
+	opening.blit (Mac_Gyver.direct,(Mac_Gyver.x, Mac_Gyver.y))
+	#pygame.display.flip() # post Mac gyver and playing
 
 	# Unit Test to permit at Mac Gyver  to catch element for his liberty
 
@@ -129,40 +127,65 @@ while continuer_open:
 		opening.blit((pygame.transform.scale(etherPNG, (30, 30))), (ether.x, ether.y))
 	if (Mac_Gyver.x, Mac_Gyver.y) == (ether.x, ether.y):
 		etherTest = False
-		opening.blit((pygame.transform.scale(etherPNG, (30, 30))), (60, 40))
+		opening.blit((pygame.transform.scale(etherPNG, (30, 30))), (800, 800))
 		
 
-	if seringueTest:
-		opening.blit((pygame.transform.scale(seringuePNG, (30, 30))), (seringue.x, seringue.y))
-	if (Mac_Gyver.x, Mac_Gyver.y) == (seringue.x, seringue.y):
-		seringueTest = False
-		opening.blit((pygame.transform.scale(seringuePNG, (30, 30))), (30, 16))
+	if tubeTest:
+		opening.blit((pygame.transform.scale(tubePNG, (30, 30))), (tube.x, tube.y))
+	if (Mac_Gyver.x, Mac_Gyver.y) == (tube.x, tube.y):
+		tubeTest = False
+		opening.blit((pygame.transform.scale(tubePNG, (30, 30))), (800, 800))
 		
 
 	if aiguilleTest:
 		opening.blit((pygame.transform.scale(aiguillePNG, (30, 30))), (aiguille.x, aiguille.y))
 	if (Mac_Gyver.x, Mac_Gyver.y) == (aiguille.x, aiguille.y):
 		aiguilleTest = False
-		opening.blit((pygame.transform.scale(aiguillePNG, (30, 30))), (50, 17))
+		opening.blit((pygame.transform.scale(aiguillePNG, (30, 30))), (800, 800))
 		
+	pygame.display.flip()
 
 	# Mac Gyver take any elements
-	if etherTest==False and seringueTest==False and aiguilleTest==False:
+	if etherTest==False and tubeTest == False and aiguilleTest == False:
 		cpt +=1
-	if cpt== 0 and home.structure[Mac_Gyver.case_y][Mac_Gyver.case_x] == 'g':
+	if cpt == 1 :
 
-		loser = True
-	if loser:
-		rectScreen = opening.get_rect()
-		#police = pygame.font.Font("led.ttf",72)
-		police = pygame.font.Font(None,30)
-		texte = police.render("Game Over -ECHAP TO QUIT",True,pygame.Color("#FFFF00"))
-		rectTexte = texte.get_rect()
-		rectTexte.center = rectScreen.center
-		opening.fill(pygame.Color("#FF0000"))
-		opening.blit(texte,rectTexte)
+		while seringueTest :
 
-		pygame.display.flip()
+			opening.blit((pygame.transform.scale(seringuePNG, (300, 300))), (0, 0))
+			for event in pygame.event.get(): #function to catch a user action	
+
+				if event.type == KEYDOWN: # catch a keyboard action
+						                  		
+					#Keys movement of Mac Gyver
+					if event.key == K_ESCAPE:
+						seringueTest = False
+
+			pygame.display.flip()
+	if cpt == 0 and home.structure[Mac_Gyver.case_y][Mac_Gyver.case_x] == 3:
+
+		
+
+		while loser :
+			rectScreen = opening.get_rect()
+			#police = pygame.font.Font("led.ttf",72)
+			police = pygame.font.Font(None,30)
+			texte = police.render("Game Over - ECHAP TO QUIT",True,pygame.Color("#FFFF00"))
+			rectTexte = texte.get_rect()
+			rectTexte.center = rectScreen.center
+			opening.fill(pygame.Color("#FF0000"))
+			opening.blit(texte,rectTexte)
+			for event in pygame.event.get(): #function to catch a user action	
+
+					if event.type == KEYDOWN: # catch a keyboard action
+							                  		
+						#Keys movement of Mac Gyver
+						if event.key == K_ESCAPE:
+							loser = False
+							continuer = False
+			pygame.display.flip()
+
+	pygame.display.flip()
 
 
 
--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import * 
+
 import random
 
 
@@ -7,22 +8,94 @@ class Support:
 	""""Create a structure" class"""
 	def __init__(self):
 		self.structure = 0
-
+		
 
 	def  create(self):
+      
+		""""Create a maze with a exhaustive exploration """
 
-		""""function to create the struct"""
+		#xMax = 7 # number of boxes  not walled 
+		taille_Max = 7 # number of boxes not walled
 
-		with open('structures.txt', "r") as fichier:
-			support_n = []
-			for row in fichier:  # w walk through each row of a structure
-	 			row_n = []
-	 			for sprite in row: #  walk through each element of row of structure
-	 				if sprite != '\n':
-	 					row_n.append(sprite) #stock element of row in a list
- 				support_n.append(row_n) #stock row of element in a list for giving a array 2D
- 			#We save a structure
-			self.structure = support_n
+		move_X = {'N':0,'S':0,'E':1,'O':-1} #  direction we take to break a wall in width
+		move_Y = {'N':1,'S':-1,'E':0,'O':0} # diretion we take to break a wall in height
+
+		# we create a matrix to stock if the state of cell of  maze  (visited or not)(0 and 1)
+		# we init all values at 0
+		# dejavu = []
+		# for y in range(taille_Max):
+		# 	for x in range(taille_Max):
+		# 		dejavu[y][x] = 0
+
+		dejavu = [[0 for x in range(taille_Max)] for y in range(taille_Max)]
+
+		#we create a labyrinth with a wall enter the boxes of maze(the goal is to break this wall 
+		#to unite all the boxes of the maze)
+
+		# we init the all values of mare at zero
+		laby = [[0 for x in range(2*taille_Max+1)] for y in range(2*taille_Max+1)]
+
+		#we put a boxes  of maze enter the wall symbolizing  by 1 = boxes and 0 = walls in matrix maze
+
+		for y in range(len(laby)-1):             #we  traverse each row of matrix maze
+			for x in range(len(laby[y])-1):       #we traverse each cells of row
+				if x % 2 != 0 and y % 2 != 0:
+					laby[y][x] = 1
+
+    	# we choice in random one cell in dejavu matrix   for begining the union of maze cell
+
+		randCell = ((random.randrange(taille_Max)), (random.randrange(taille_Max)))
+		lastCell = [(randCell)]
+
+		#we make a while loop to visit all boxes of maze for the union.
+		while lastCell != []:
+			(x, y) = lastCell[-1] # we take the last element of the pile
+			dejavu[y][x] = 1 
+			lNeighboors = []
+			for move in ['N','S','O','E']:
+				x_1 = x + move_X[move] #the width of the cell we use to make a union
+				y_1 = y + move_Y[move] #the height of the cell we use to make a union
+
+
+				if x_1 >= 0 and x_1 < taille_Max and y_1 >= 0 and y_1 < taille_Max:
+					
+					if dejavu[y_1][x_1] == 0: #we check if a boxe are not visited 
+
+						lNeighboors.append(move)
+
+
+			if len(lNeighboors) > 0:
+				var = random.choice(lNeighboors) # we choice randomly a neighboors boxe to make a union 
+
+	            #In the maze this position are((2x+1,2y+1),(2x_1+1,2y_1+1), middle of the two cells(wall) = (x +x_1+1,y,y_1+1),
+	            #the method is to break this wall to unit the two boxes of maze
+				laby[x+x+move_X[var]+1][y+y+move_Y[var]+1] = 1
+	            # we udpate the position of boxes to continue the maze boxes union
+				x, y = x + move_X[var], y + move_Y[var]	
+
+				lastCell.append((x, y))
+			else:
+				lastCell.pop()
+		laby[1][1] = 2
+		laby[2*taille_Max-1][2*taille_Max-1] = 3
+		self.structure = laby
+
+
+
+
+		# """"function to create the struct"""
+
+		# with open('structures.txt', "r") as fichier:
+		# 	support_n = []
+		# 	for row in fichier:  # w walk through each row of a structure
+	 # 			row_n = []
+	 # 			for sprite in row: #  walk through each element of row of structure
+	 # 				if sprite != '\n':
+	 # 					row_n.append(sprite) #stock element of row in a list
+ 	# 			support_n.append(row_n) #stock row of element in a list for giving a array 2D
+ 	# 		#We save a structure
+		# #self.structure = support_n
+
 
 
 	def poster(self, opening):
@@ -39,16 +112,19 @@ class Support:
 			num_case = 0
 			for sprite in row:
 			#we calcul the elemnent tail in pixels
+				#begin = []
 				x = num_case * tail_sprite
 				y = num_row * tail_sprite
-				if sprite == '1':		   #w = Wall(built Wall)
+				if sprite == 0:		   #w = Wall(built Wall)
 					opening.blit(pygame.transform.scale(wall,(30, 30)),(x,y))
 				
-				elif sprite == 'g':		   #g = goal(built goal)
+				elif sprite == 3:		   #g = goal(built goal)
 					opening.blit(pygame.transform.scale(goal,(30, 30)), (x,y))
-		
+				#elif sprite == 2:		   #g = goal(built goal)
+					#opening.blit(pygame.transform.scale((pygame.image.load('MacGyver.png').convert()),(30, 30)), (x,y))
 				num_case += 1
 			num_row += 1
+			
 
 class Element:	
 	""""Create a element clas for kill a garden """
@@ -69,11 +145,11 @@ class Element:
 		while self.possible:
 
 			
-			self.case_x = random.randint(0,14) # generate randomly a number
-			self.case_y = random.randint(0,14) #generate randomly a number
-			position_create = self.home.structure[self.case_x][self.case_y] #value to know if we have a wall or no
+			self.case_x = random.randint(0,9) # generate randomly a number
+			self.case_y = random.randint(0,9) #generate randomly a number
+			position_create = self.home.structure[self.case_y][self.case_x] #value to know if we have a wall or no
 		
-			if position_create == '0' :
+			if position_create == 1:
 				self.y = self.case_y * 30  # We define/accept the position for the object
 				self.x = self.case_x * 30
 				self.possible = False  # We construct the element once 
@@ -87,17 +163,18 @@ class Maestro:
 		
 	def __init__(self,home):	#constructor class
 
-		self.direct = pygame.transform.scale((pygame.image.load('MacGyver.png').convert()),(30,30))
-
-		self.case_x = 0
-		self.case_y = 0
-		self.x = 0
-		self.y = 0
+		
+		self.case_x = 1
+		self.case_y = 1
+		self.x = 30
+		self.y = 30
 		self.home = home
 		#self.e = e
-		self.cpt = 0
-		
-		
+		#self.cpt = 0
+		self.direct = pygame.transform.scale((pygame.image.load('MacGyver.png').convert()),(30, 30))
+		#self.directory = self.direct
+		#self.direct = self.home.indic #pygame.transform.scale((pygame.image.load('MacGyver.png').convert()),(30,30))
+
 	
 		
 
@@ -106,40 +183,42 @@ class Maestro:
 
 		tail_sprite = 30
 		nombre_sprite_cote = 15
+		
+		
 
 		#move to right
 		if directory == 'right':
 			#For don't go out
 			if self.case_x < (nombre_sprite_cote - 1):
 				#We verify if they don't have Wall
-				if self.home.structure[self.case_y][self.case_x+1] != '1':
+				if self.home.structure[self.case_y][self.case_x+1] != 0:
 					#move to one hut
 					self.case_x += 1
 					#Calculate real position Mac Gyver position
 					self.x = self.case_x * tail_sprite
-			
-
+			#self.directory = sdirect
 		#move to left
 		if directory == 'left':
 			if self.case_x > 0:
-				if self.home.structure[self.case_y][self.case_x-1] != '1':
+				if self.home.structure[self.case_y][self.case_x-1] != 0:
 					self.case_x -= 1
 					self.x = self.case_x * tail_sprite
-			
+			#self.directory = pygame.transform.scale((pygame.image.load('MacGyver.png').convert()),(30, 30))
 		
 		#move to top
 		if directory == 'top':
 			if self.case_y > 0:
-				if self.home.structure[self.case_y-1][self.case_x] != '1':
+				if self.home.structure[self.case_y-1][self.case_x] != 0:
 					self.case_y -= 1
 					self.y = self.case_y * tail_sprite
-			
+			#self.directory = self.direct
 		#move to low
 		if directory == 'low':
 			if self.case_y < (nombre_sprite_cote - 1):
-				if self.home.structure[self.case_y+1][self.case_x] != '1':
+				if self.home.structure[self.case_y+1][self.case_x] != 0:
 					self.case_y += 1
 					self.y = self.case_y * tail_sprite
+			#self.directory = pygame.transform.scale((pygame.image.load('MacGyver.png').convert()),(30, 30))
 
 	
 
